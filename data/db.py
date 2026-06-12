ed77d74d828112fe310980cead51c682ac9622cf
tony-pythony123 <lgentile@studenti.apuliadigital.it>
2026-06-12 10:31:13 +0200

    aggiunto dbname getenv

--- data/db.py
+++ data/db.py
@@ -4,7 +4,7 @@ import os
 def get_connection():
     return psycopg2.connect(
         host=os.getenv("DB_HOST", "localhost"),
-        dbname="db_gestione-consegne",
+        dbname=os.getenv("DB_NAME", "db_gestione-consegne"),
         user=os.getenv("DB_USER", "postgres"),
         password=os.getenv("DB_PASSWORD", "Bari2025"),
         port=os.getenv("DB_PORT", 8082)
