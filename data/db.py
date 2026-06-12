9989578f7952f051b2a5674619f755c4214145a2
tony-pythony123 <lgentile@studenti.apuliadigital.it>
2026-06-12 14:29:46 +0200

    sistemato db.py e aggiunto .env a .dockerignore

--- data/db.py
+++ data/db.py
@@ -2,10 +2,16 @@ import psycopg2
 import os
 
 def get_connection():
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+
+    if not user or not password:
+        raise ValueError("Credenziali database non configurate correttamente")
+
     return psycopg2.connect(
         host=os.getenv("DB_HOST", "localhost"),
         dbname=os.getenv("DB_NAME", "db_gestione-consegne"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", "Bari2025"),
+        user=user,
+        password=password,
         port=os.getenv("DB_PORT", 8082)
     )
