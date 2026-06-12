feac523cfa6b0a12185681d0d94397a7568cea00
tony-pythony123 <lgentile@studenti.apuliadigital.it>
2026-06-12 16:21:31 +0200

    nascosto dbname

--- data/db.py
+++ data/db.py
@@ -4,13 +4,15 @@ import os
 def get_connection():
     user = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
+    dbname = os.getenv("DB_NAME")
 
-    if not user or not password:
+
+    if not user or not password or not dbname:
         raise ValueError("Credenziali database non configurate correttamente")
 
     return psycopg2.connect(
         host=os.getenv("DB_HOST", "localhost"),
-        dbname=os.getenv("DB_NAME", "db_gestione-consegne"),
+        dbname=dbname,
         user=user,
         password=password,
         port=os.getenv("DB_PORT", 8082)
